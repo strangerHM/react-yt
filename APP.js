5721c3e4c1b3f85758c8f5c67cf5b3f6d0731d70
strangerHM <mazharhadiya18@gmail.com>
2026-03-27 15:47:24 +0500

    01-folder done

---
+++ APP.js
@@ -0,0 +1,9 @@
+
+/* export -2
+named - export multiple things with specific names
+default - export only one thing */
+
+const a = 20
+export const age = 90
+export const arr= [ 1,2,3,4,5]
+export default a
